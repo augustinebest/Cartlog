8ed65be2eaa2412688edae900945f7291a3acf64
Igwe Augustine <augustusbest00@gmail.com>
2018-12-11 10:30:19 -0900

    'nice'

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,21 +1,97 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import { Alert } from 'reactstrap';
+import Loader from '../Loader/Loader';
+import { Url } from '../Factories';
+import validate from '../validate';
+import axios from 'axios';
 import './Auth.css'
 
 class Login extends React.Component {
     constructor(props) {
         super(props)
-
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            visible: true,
+            color: 'danger',
+            error: null
+        }
     }
-    onSubmit = () => {
-
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
     }
-    handleChange = () => {
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.canSubmit();
+    }
 
+    canSubmit = () => {
+        const { email, password } = this.state;
+        console.log(email, password);
+        this.setState({
+            loading: true
+        })
+        if(!validate('email', email)) {
+            this.setState({
+                error: 'Please use a valid email',
+                visible: true,
+                loading: false,
+            })
+        } else if(!validate('password', password)) {
+            this.setState({
+                error: 'Please use a valid password',
+                visible: true,
+                loading: false,
+            })
+        } else  {
+            axios.post(`${Url}/login`, {
+                email: email,
+                password: password
+            }, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => {
+                console.log(res);
+                var result = res.data;
+                if (result.res === true) {
+                    this.setState({
+                        error: 'Successfully logged in',
+                        visible: true,
+                        loading: false,
+                        color: 'success'
+                    })
+                } else if(result.message === 'failed') {
+                    this.setState({
+                        error: 'Your email or Password is incorrect',
+                        visible: true,
+                        loading: false,
+                    })
+                } else if (result.message === 'non_active') {
+                    this.setState({
+                        error: 'Your email or Password is incorrect',
+                        visible: true,
+                        loading: false,
+                    })
+                } else {
+                    this.setState({
+                        error: 'Sorry an error occured',
+                        visible: true,
+                        loading: false,
+                    })
+                }
+            })
+        }
     }
 
     render() {
+        const { loading, error, visible, color } = this.state;
         const style = {
             position: 'relative',
             width: '100%',
@@ -31,6 +107,12 @@ class Login extends React.Component {
                     <div className='login_auth_page'>
                     <h3>Hello! welcome back</h3>
                     <p>Enter your details below.</p>
+                    {
+                            error &&
+                            <Alert color={color} isOpen={visible} toggle={this.onDismiss}>
+                                {error}
+                            </Alert>
+                        }
                         <form style={{ marginTop: '80px' }} onSubmit={this.onSubmit}>
                             <div style={style} className='ui labeled input box'>
                                 <div className='ui label label'><i className="fa fa-envelope"></i></div>
@@ -48,7 +130,12 @@ class Login extends React.Component {
                                     onChange={this.handleChange}
                                     required />
                             </div>
-                            <button type='submit' className='login__button'>SIGN IN</button> <NavLink to='/forgot-password'>Forgot password?</NavLink>
+                            <button type='submit' className={loading ? 'login__button1' : 'login__button'}>SIGN IN
+                            {
+                                    loading &&
+                                    <Loader />
+                                }
+                            </button> <NavLink to='/forgot-password'>Forgot password?</NavLink>
                         </form>
                     </div>
                 </div>
